a621e57bbfe860eb4b33f4e78bc951d471892a79
George Hill <g.hill@mac.com>
2020-06-29 17:27:42 -0700

    Standardize on two spaces for indentation and fix leading and trailing blank lines.

--- index.js
+++ index.js
@@ -27,4 +27,3 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
-
