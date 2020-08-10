ace2932da30ed0e600718cd550001f9ae96d9aae
asching7108 <asching7108@gmail.com>
2020-08-10 18:10:03 -0400

    Clean up old config files and add example.env file

--- config~template.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  host            : 'classmysql.engr.oregonstate.edu', // Database host.
-  user            : 'cs361_*****', // Database user.
-  password        : '*****', // Database user's password.
-  database        : 'cs361_*****', // Database name.
-  JWT_SECRET      : 'e3c29e36-97c3-4388-98e4-4e0664099ef5', // Long string of random characters.
-  appSecret       : '7C44-74D44-WppQ3877S', // Long string of random characters.
-  cryptoSecret    : 'secret key 123', // Long string of random characters.
-};
